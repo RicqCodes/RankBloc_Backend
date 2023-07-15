756235f322f60cfff7d074d76ebea1c699e0764e
ricqCodes <princenwakanma1996@gmail.com>
2023-07-15 12:00:59 +0100

    Completed signature verification middleware

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,32 @@
 import { NextFunction, Request, Response } from "express";
 
 import { catchAsync } from "../utils/catchAsync";
+import User from "../models/userModel";
+import AppError from "../utils/appError";
 
-export const createUser = catchAsync(async (req: Request, res: Response) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined",
-  });
-});
+export const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { address, signedMessage } = req.body;
+
+    const existingUser = await User.findOne({ publicAddress: address });
+
+    // Check if user exists
+    if (existingUser) {
+      next(new AppError("User already exists", 409));
+    }
+
+    // Save new user to the  database
+    const user = new User();
+    user.publicAddress = address;
+    user.signedMessage = signedMessage;
+    await user.save();
+
+    res.status(201).json({
+      status: "success",
+      message: "Account Created",
+    });
+  }
+);
 
 export const getAllUsers = catchAsync(async (req: Request, res: Response) => {
   res.status(500).json({
